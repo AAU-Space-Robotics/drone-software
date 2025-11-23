45f76361376be6c834d06fdce7aa3fb812a60fa4
Magnus Daroe <mdaro22@student.aau.dk>
2025-11-23 17:01:46 +0100

    Best drone

--- src/fc_interface/src/fci_controller.cpp
+++ src/fc_interface/src/fci_controller.cpp
@@ -151,7 +151,7 @@ Eigen::Vector4d FCI_Controller::velocityControl(double sample_time,
                        attitude_pid_gains_.pitch.Kd * velocity_error_frd_d.x();
 
     double yaw_cmd = 0.0; // Yaw command
-    double hover_thrust_estimate_ = -0.5; // Estimated hover thrust. Should be made adaptive later.
+    double hover_thrust_estimate_ = -0.7; // Estimated hover thrust. Should be made adaptive later.
 
     double thrust_cmd = hover_thrust_estimate_ + attitude_pid_gains_.thrust.Kp * velocity_error_frd.z() +
                         attitude_pid_gains_.thrust.Ki * integral_velocity_error_frd.z() +
